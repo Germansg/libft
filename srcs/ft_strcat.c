43a82a359859b0a31a5d445f51ad3ad1d8a71ef9
Germansg <osiris129@sbcglobal.net>
2017-03-04 18:11:42 -0800

    added ft_strchr

--- srcs/ft_strcat.c
+++ srcs/ft_strcat.c
@@ -2,20 +2,20 @@
 
 char *ft_strcat(char *s1, const char *s2)
 {
-  int i;
-  int j;
+	int i;
+	int j;
 
-  i = 0;
-  j = 0;
-  while (s1[i])
-    i++;
-  while (s2[j])
-  {
-    s1[i] = s2[j];
-    i++;
-    j++;
-  }
-  i++;
-  s1[i] = '\0';
-  return (s1);
+	i = 0;
+	j = 0;
+	while (s1[i])
+		i++;
+	while (s2[j])
+	{
+		s1[i] = s2[j];
+		i++;
+		j++;
+	}
+	i++;
+	s1[i] = '\0';
+	return (s1);
 }
