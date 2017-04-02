7e619cf5e8b938d2a00696385eb5a7558b6f55fb
Germansg <osiris129@sbcglobal.net>
2017-04-01 17:08:01 -0700

    fixed strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: ggladkov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/21 21:17:48 by ggladkov          #+#    #+#             */
-/*   Updated: 2017/03/26 18:44:50 by ggladkov         ###   ########.fr       */
+/*   Updated: 2017/04/01 17:05:42 by ggladkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,15 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	unsigned int	i;
 	char			*new;
-	char			size;
+	int			size;
 
 	i = 0;
-	if (!s || !f || !i)
-		return (NULL);	
 	size = 0;
-	while (s[i++])
+	while (s[i])
+	{
+		i++;
 		size++;
+	}
 	i = 0;
 	new = (char *)malloc(sizeof(char) * (size + 1));
 	if (!new)
